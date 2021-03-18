21028730d95c5de65bba3c3837178543c69e71d9
Judeoddeus <julianaacademi@gmail.com>
2021-03-17 22:17:11 -0300

    atualização dos repositórios

---
+++ site_starbucks/js/funcoes.js
@@ -0,0 +1,13 @@
+function TrocarImagem(imagemNova){
+    document.querySelector(".starbucks").src = imagemNova;
+}
+
+function MudarCirculo(corNova){
+    let circulo = document.querySelector(".circulo");
+    circulo.style.background = corNova;
+}
+
+function CorLogo(){
+    let corLogo = document.querySelector("altera-cor");
+    caixa-texto.a.style
+}
